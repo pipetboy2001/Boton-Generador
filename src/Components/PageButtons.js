c421d92ff603e011a7f3cb7e30cc5ee7de8c9506
Felipe Fernandez <FelipeFelizx@gmail.com>
2022-12-28 02:54:45 -0300

    Feature: Se añade title a cada boton
    <div title=' ' > button </div>

--- src/Components/PageButtons.js
+++ src/Components/PageButtons.js
@@ -11,22 +11,28 @@ import { BsFillExclamationDiamondFill } from "react-icons/bs";
 export const PageButtons = () => {
     return (
         <>
-        <center>
+            <center>
                 <div className="container-class">
                     <center><h2 className='Titulo'>Botones</h2></center>
                     <div className='BotonesBasicos'>
                         <div id="item-0">
                             <p>Boton base</p>
-                            <Button>Click me!</Button>
+                            <div title="<Button />">
+                                <Button >Click me!</Button>
+                            </div>
                         </div>
                         <div id="item-1">
                             <p>Boton con contorno</p>
-                            <Button variant="outline">Click me!</Button>
+                            <div title='<Button variant="outline" />'>
+                                <Button variant="outline">Click me!</Button>
+                            </div>
                         </div>
                         <div id="item-2">
                             {/* Añadir boton con texto */}
                             <p>Boton con texto</p>
-                            <Button variant="text">Click me!</Button>
+                            <div title='<Button variant="text" />'>
+                                <Button variant="text">Click me!</Button>
+                            </div>
                         </div>
                     </div>
 
@@ -34,24 +40,32 @@ export const PageButtons = () => {
                         <div id="item-0">
                             {/* Añadir boton deshabilitado */}
                             <p>Boton deshabilitado</p>
-                            <Button className="button-disabled" disabled>Click me!</Button >
-
+                            <div title='<Button disabled />'>
+                                <Button className="button-disabled" disabled>Click me!</Button >
+                            </div>
 
                         </div>
                         <div id="item-1">
                             {/* Añadir boton disabled Shadow*/}
                             <p>Boton deshabilitado con sombra</p>
-                            <Button className="button-disabled-shadow"  disabledShadow>Click me!</Button>
+                            <div title='<Button disabledShadow />'>
+                                <Button className="button-disabled-shadow" disabledShadow>Click me!</Button>
+                            </div>
                         </div>
                         <div id="item-2">
                             {/* Añadir boton con contornor deshabilitado */}
                             <p>Boton con contorno deshabilitado</p>
-                            <Button variant="outline"  disabled>Click me!</Button>
+                            <div title='<Button disable variant="outline">'>
+                                <Button variant="outline" disabled>Click me!</Button>
+                            </div>
+
                         </div>
                         <div id="item-3">
                             {/* Añadir boton con texto deshabilitado */}
                             <p>Boton con texto deshabilitado</p>
-                            <Button variant="text"  disabled>Click me!</Button>
+                            <div title='<Button disable variant="text">'>
+                                <Button variant="text" disabled>Click me!</Button>
+                            </div>
                         </div>
                     </div>
 
@@ -59,23 +73,34 @@ export const PageButtons = () => {
                         {/* Añadir boton con iconos */}
                         <div id='item-0'>
                             <p>Boton con icono</p>
+                            <div title='<Button icon>'>
+
+                            </div>
                             <Button>Click me! <BsFillBookmarkStarFill /></Button>
                         </div>
                         <div id='item-1'>
                             <p>Boton con icono y contorno</p>
-                            <Button variant="outline">Click me! <BsFillExclamationTriangleFill /></Button>
+                            <div title='<Button icon variant="outline">'>
+                                <Button variant="outline">Click me! <BsFillExclamationTriangleFill /></Button>
+                            </div>
                         </div>
                         <div id='item-2'>
                             <p>Boton con icono y texto</p>
-                            <Button variant="text">Click me! <BsFillExclamationOctagonFill /></Button>
+                            <div title='<Button icon variant="text">'>
+                                <Button variant="text">Click me! <BsFillExclamationOctagonFill /></Button>
+                            </div>
                         </div>
                         <div id='item-3'>
                             <p>Boton con icono deshabilitado</p>
-                            <Button disabled>Click me! <BsFillExclamationCircleFill /></Button>
+                            <div title='<Button icon disabled>'>
+                                <Button disabled>Click me! <BsFillExclamationCircleFill /></Button>
+                            </div>
                         </div>
                         <div id='item-4'>
                             <p>Boton con icono deshabilitado con sombra</p>
-                            <Button disabledShadow>Click me! <BsFillExclamationDiamondFill /></Button>
+                            <div title='<Button icon disabledShadow>'>
+                                <Button disabledShadow>Click me! <BsFillExclamationDiamondFill /></Button>
+                            </div>
                         </div>
                     </div>
 
@@ -83,15 +108,21 @@ export const PageButtons = () => {
                         {/* Añadir botones con distintos tamaños */}
                         <div id="item-0">
                             <p>Pequeño</p>
-                            <Button size="sm">Click me!</Button>
+                            <div title='<Button size="sm">'>
+                                <Button size="sm">Click me!</Button>
+                            </div>
                         </div>
                         <div id="item-1">
                             <p>Mediano</p>
-                            <Button size="md">Click me!</Button>
+                            <div title='<Button size="md">'>
+                                <Button size="md">Click me!</Button>
+                            </div>
                         </div>
                         <div id="item-2">
                             <p>Grande</p>
-                            <Button size="lg">Click me!</Button>
+                            <div title='<Button size="lg">'>
+                                <Button size="lg">Click me!</Button>
+                            </div>
                         </div>
                     </div>
 
@@ -99,23 +130,31 @@ export const PageButtons = () => {
                         {/* Añadir botones con distintos colores */}
                         <div id="item-0">
                             <p>Color por defecto</p>
-                            <Button color="default">Click me!</Button>
+                            <div title='<Button color="default">'>
+                                <Button color="default">Click me!</Button>
+                            </div>
                         </div>
                         <div id="item-1">
                             <p>Color primario</p>
-                            <Button color="primary">Click me!</Button>
+                            <div title='<Button color="primary">'>
+                                <Button color="primary">Click me!</Button>
+                            </div>
                         </div>
                         <div id="item-2">
                             <p>Color secundario</p>
-                            <Button color="secondary">Click me!</Button>
+                            <div title='<Button color="secondary">'>
+                                <Button color="secondary">Click me!</Button>
+                            </div>
                         </div>
                         <div id="item-3">
                             <p>Color terciario</p>
-                            <Button color="danger">Click me!</Button>
+                            <div title='<Button color="tertiary">'>
+                                <Button color="danger">Click me!</Button>
+                            </div>
                         </div>
                     </div>
                 </div>
-        </center>
+            </center>
         </>
     )
 }
