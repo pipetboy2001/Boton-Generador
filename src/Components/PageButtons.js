acb49980b0abca65943d1a940742078ae54329aa
Felipe Fernandez <FelipeFelizx@gmail.com>
2022-12-28 19:57:17 -0300

    Fix: Arreglar grilla

--- src/Components/PageButtons.js
+++ src/Components/PageButtons.js
@@ -21,27 +21,27 @@ export const PageButtons = () => {
                     <center><h2 className='Titulo'>Botones</h2></center>
                     <div className='BotonesBasicos'>
                         <div id="item-0">
-                            <p>Boton base</p>
+                            <p>Botón base</p>
                             <div title="<Button />">
                                 <Button >Click me!</Button>
                             </div>
                         </div>
                         <div id="item-1">
-                            <p>Boton con contorno</p>
+                            <p>Botón con contorno</p>
                             <div title='<Button variant="outline" />'>
                                 <Button variant="outline">Click me!</Button>
                             </div>
                         </div>
                         <div id="item-2">
                             {/* Añadir boton con texto */}
-                            <p>Boton con texto</p>
+                            <p>Botón con texto</p>
                             <div title='<Button variant="text" />'>
                                 <Button variant="text">Click me!</Button>
                             </div>
                         </div>
                         <div id="item-3">
                             {/* Añadir boton con sombra */}
-                            <p>Boton con sombra</p>
+                            <p>Botón con sombra</p>
                             <div title='<Button shadow />'>
                                 <Button shadow>Click me!</Button>
                             </div>
@@ -51,7 +51,7 @@ export const PageButtons = () => {
                     <div className='BotonesDesahbilitar'>
                         <div id="item-0">
                             {/* Añadir boton deshabilitado */}
-                            <p>Boton deshabilitado</p>
+                            <p>Botón deshabilitado</p>
                             <div title='<Button disabled />'>
                                 <Button disabled>Click me!</Button >
                             </div>
@@ -59,14 +59,14 @@ export const PageButtons = () => {
                         </div>
                         <div id="item-1">
                             {/* Añadir boton disabled Shadow*/}
-                            <p>Boton deshabilitado con sombra</p>
+                            <p>Botón deshabilitado con sombra</p>
                             <div title='<Button disabledShadow />'>
-                                <Button  disabledShadow>Click me!</Button>
+                                <Button disabledShadow>Click me!</Button>
                             </div>
                         </div>
                         <div id="item-2">
                             {/* Añadir boton con contornor deshabilitado */}
-                            <p>Boton con contorno deshabilitado</p>
+                            <p>Botón con contorno deshabilitado</p>
                             <div title='<Button disable variant="outline">'>
                                 <Button variant="outline" disabled>Click me!</Button>
                             </div>
@@ -74,7 +74,7 @@ export const PageButtons = () => {
                         </div>
                         <div id="item-3">
                             {/* Añadir boton con texto deshabilitado */}
-                            <p>Boton con texto deshabilitado</p>
+                            <p>Botón con texto deshabilitado</p>
                             <div title='<Button disable variant="text">'>
                                 <Button variant="text" disabled>Click me!</Button>
                             </div>
@@ -84,25 +84,25 @@ export const PageButtons = () => {
                     <div className='BotonesIconos'>
                         {/* Añadir boton con iconos */}
                         <div id='item-0'>
-                            <p>Boton con icono</p>
+                            <p>Botón con icono</p>
                             <div title='<Button icon>'>
                                 <Button icon={<BsFillBookmarkStarFill />} iconPosition="left" > Click me! </Button>
                             </div>
                         </div>
                         <div id='item-1'>
-                            <p>Boton con icono y contorno</p>
+                            <p>Botón con icono y contorno</p>
                             <div title='<Button icon variant="outline">'>
                                 <Button variant="outline" icon={<BsFillExclamationTriangleFill />} iconPosition="right"> Click me! </Button>
                             </div>
                         </div>
                         <div id='item-2'>
-                            <p>Boton con icono y texto</p>
+                            <p>Botón con icono y texto</p>
                             <div title='<Button icon variant="text">'>
                                 <Button variant="text" icon={< BsFillExclamationOctagonFill />} iconPosition="left"> Click me! </Button>
                             </div>
                         </div>
                         <div id='item-3'>
-                            <p>Boton con icono deshabilitado</p>
+                            <p>Botón con icono deshabilitado</p>
                             <div title='<Button icon disabled>'>
                                 <Button disabled icon={<BsFillExclamationCircleFill />} iconPosition="right"> Click me! </Button>
                             </div>
@@ -159,61 +159,60 @@ export const PageButtons = () => {
                         </div>
                     </div>
 
-                    <center>
-                        <h2 className='Titulo'>Modificable</h2>
-                        <Button variant={variant} disabled={disabled} size={size} color={color} shadow={shadow}>Click me!</Button>
-                        <div>
-                            <label>
-                                Variant:
-                                <select value={variant} onChange={event => setVariant(event.target.value)}>
-                                    <option value="default">Default</option>
-                                    <option value="outlined">Outlined</option>
-                                    <option value="text">Text</option>
-                                </select>
-                            </label>
-                        </div>
-                        <div>
-                            <label>
-                                Disabled:
-                                <input type="checkbox" checked={disabled} onChange={event => setDisabled(event.target.checked)} />
-                            </label>
-                        </div>
-                        <div>
-                            <label>
-                                Shadow:
-                                <input type="checkbox" checked={shadow} onChange={event => setShadow(event.target.checked)} />
-                            </label>
-                        </div>
-                        <div>
-                            <label>
-                                Color:
-                                <select value={color} onChange={event => setColor(event.target.value)}>
-                                    <option value="default">Default</option>
-                                    <option value="primary">Primary</option>
-                                    <option value="secondary">Secondary</option>
-                                    <option value="danger">Danger</option>
-                                </select>
+                    <h2 className='Titulo'>Modificable</h2>
+                    <center className='BotonModificable'>
+                        <div id="item-0">
+                            <Button variant={variant} disabled={disabled} size={size} color={color} shadow={shadow}>Click me!</Button>
 
-                            </label>
                         </div>
-                        <div>
-                            <label>
-                                Size:
-                                <select value={size} onChange={event => setSize(event.target.value)}>
-                                    <option value="
+                        <div id='item-1'>
+                            <div>
+                                <label>
+                                    Variante:
+                                    <select value={variant} onChange={event => setVariant(event.target.value)}>
+                                        <option value="default">Default</option>
+                                        <option value="outlined">Outlined</option>
+                                        <option value="text">Text</option>
+                                    </select>
+                                </label>
+                            </div>
+                            <div>
+                                <label className='Checkbox'>
+                                    Disabilitado:
+                                    <input type="checkbox" checked={disabled} onChange={event => setDisabled(event.target.checked)} />
+                                </label>
+                            </div>
+                            <div>
+                                <label className='Checkbox'>
+                                    Sombra:
+                                    <input type="checkbox" checked={shadow} onChange={event => setShadow(event.target.checked)} />
+                                </label>
+                            </div>
+                            <div>
+                                <label>
+                                    Color:
+                                    <select value={color} onChange={event => setColor(event.target.value)}>
+                                        <option value="default">Default</option>
+                                        <option value="primary">Primary</option>
+                                        <option value="secondary">Secondary</option>
+                                        <option value="danger">Danger</option>
+                                    </select>
+
+                                </label>
+                            </div>
+                            <div>
+                                <label>
+                                    Tamaño:
+                                    <select value={size} onChange={event => setSize(event.target.value)}>
+                                        <option value="
                                     ">Default</option>
-                                    <option value="sm">Small</option>
-                                    <option value="md">Medium</option>
-                                    <option value="lg">Large</option>
-                                </select>
-                            </label>
+                                        <option value="sm">Small</option>
+                                        <option value="md">Medium</option>
+                                        <option value="lg">Large</option>
+                                    </select>
+                                </label>
+                            </div>
                         </div>
-
-
-
-
-
-
                     </center>
 
                 </div>
